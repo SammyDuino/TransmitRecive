01d03c88a2c3a826932b33482f3d0d8ed186ff9d
PacksGamingHD <craftyswans@gmail.com>
2015-04-20 11:09:20 -0700

    Update Rx.ino

--- Rx/Rx.ino
+++ Rx/Rx.ino
@@ -4,11 +4,45 @@
 //http://www.genotronex.com/
 //..................................
 #include <VirtualWire.h>
+
+const int buzzerPin = 8;
+const int songLength = 18;
+char notes[] = "cdfda ag cdfdg gf ";
+int beats[] = {1,1,1,1,1,1,4,4,2,1,1,1,1,1,1,4,4,2};
+int tempo = 150;
+
+void allThatJunk()
+{
+    int i, duration;
+  
+  for (i = 0; i < songLength; i++) // step through the song arrays
+  {
+    duration = beats[i] * tempo;  // length of note/rest in ms
+    
+    if (notes[i] == ' ')          // is this a rest? 
+    {
+      delay(duration);            // then pause for a moment
+    }
+    else                          // otherwise, play the note
+    {
+      tone(buzzerPin, frequency(notes[i]), duration);
+      delay(duration);            // wait for tone to finish
+    }
+    delay(tempo/10);              // brief pause between notes
+  }
+  
+  // We only want to play the song once, so we'll pause forever:
+
+  // If you'd like your song to play over and over,
+  // remove the above statement
+}
+
+#include <VirtualWire.h>
 void setup()
 {
     vw_set_ptt_inverted(true); // Required for DR3100
     vw_set_rx_pin(12);
-    vw_setup(300);  // Bits per sec
+    vw_setup(4000);  // Bits per sec
     pinMode(13, OUTPUT);
 
     vw_rx_start();       // Start the receiver PLL running
@@ -20,19 +54,48 @@ void setup()
 
     if (vw_get_message(buf, &buflen)) // Non-blocking
     {
-      
-      digitalWrite(13,1);
-     /* if(buf[0]=='1'){
+      if(buf[0]=='1'){
 
   
    digitalWrite(13,1);
       }  
    if(buf[0]=='0'){
   digitalWrite(13,0);
-    }*/
-    delay(1000);
-    digitalWrite(13,0);
+    }
 
 }
-delay(200);
+}
+ 
+
+int frequency(char note) 
+{
+  // This function takes a note character (a-g), and returns the
+  // corresponding frequency in Hz for the tone() function.
+  
+  int i;
+  const int numNotes = 8;  // number of notes we're storing
+  
+  // The following arrays hold the note characters and their
+  // corresponding frequencies. The last "C" note is uppercase
+  // to separate it from the first lowercase "c". If you want to
+  // add more notes, you'll need to use unique characters.
+
+  // For the "char" (character) type, we put single characters
+  // in single quotes.
+
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523};
+  
+  // Now we'll search through the letters in the array, and if
+  // we find it, we'll return the frequency for that note.
+  
+  for (i = 0; i < numNotes; i++)  // Step through the notes
+  {
+    if (names[i] == note)         // Is this the one?
+    {
+      return(frequencies[i]);     // Yes! Return the frequency
+    }
+  }
+  return(0);  // We looked through everything and didn't find it,
+              // but we still need to return a value, so return 0.
 }
